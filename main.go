4ec8d157d927463c733ae450e20c6d348476ebdc
Chris Ramón <rchristian.ramon@gmail.com>
2025-02-05 14:26:07 -0500

    main: replaces cobra by bubbletea scaffolding

--- main.go
+++ main.go
@@ -1,24 +1,79 @@
 package main
 
 import (
-  "fmt"
-  "strings"
+	"fmt"
+	"os"
+	"strings"
 
-  "github.com/spf13/cobra"
+	tea "github.com/charmbracelet/bubbletea"
 )
 
-func main() {
-  compatibility := &cobra.Command{
-    Use:   "compatibility",
-    Short: "Check compatibility validation against the GraphQL standard reference implementation.",
-    Long: `This command allows you to compare a GraphQL implementation against the official reference implementation.`,
-    Args: cobra.MinimumNArgs(1),
-    Run: func(cmd *cobra.Command, args []string) {
-      fmt.Println("Print: " + strings.Join(args, " "))
-    },
-  }
+var choices = []string{"https://github.com/graphql-go/graphql"}
 
-  var rootCmd = &cobra.Command{Use: "app"}
-  rootCmd.AddCommand(compatibility)
-  rootCmd.Execute()
+type model struct {
+	cursor int
+	choice string
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "q", "esc":
+			return m, tea.Quit
+
+		case "enter":
+			m.choice = choices[m.cursor]
+			return m, tea.Quit
+
+		case "down", "j":
+			m.cursor++
+			if m.cursor >= len(choices) {
+				m.cursor = 0
+			}
+
+		case "up", "k":
+			m.cursor--
+			if m.cursor < 0 {
+				m.cursor = len(choices) - 1
+			}
+		}
+	}
+
+	return m, nil
+}
+
+func (m model) View() string {
+	s := strings.Builder{}
+	s.WriteString("")
+
+	for i := 0; i < len(choices); i++ {
+		if m.cursor == i {
+			s.WriteString("(•) ")
+		} else {
+			s.WriteString("( ) ")
+		}
+		s.WriteString(choices[i])
+		s.WriteString("\n")
+	}
+	s.WriteString("\n(press q to quit)\n")
+
+	return s.String()
+}
+
+func main() {
+	p := tea.NewProgram(model{})
+
+	m, err := p.Run()
+	if err != nil {
+		os.Exit(1)
+	}
+
+	if m, ok := m.(model); ok && m.choice != "" {
+		fmt.Printf("\n---\nYou chose %s!\n", m.choice)
+	}
 }

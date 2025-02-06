84d6df1890febd8994edd2edb4531c1b0b931e87
Chris Ramón <rchristian.ramon@gmail.com>
2025-02-06 14:29:37 -0500

    puller: adds github client wiring

--- puller/puller.go
+++ puller/puller.go
@@ -1,5 +1,12 @@
 package puller
 
+import (
+	"log"
+	"os"
+
+	"github.com/google/go-github/v69/github"
+)
+
 type Puller struct {
 }
 
@@ -9,6 +16,15 @@ type PullerResult struct {
 type PullerParams struct {
 }
 
-func (p *Puller) Pull(p *PullerParams) *PullerResult {
-  return &PullerResult{}
+func (p *Puller) Pull(params *PullerParams) (*PullerResult, error) {
+	token := os.Getenv("GITHUB_AUTH_TOKEN")
+	if token == "" {
+		return nil, nil
+	}
+
+	client := github.NewClient(nil).WithAuthToken(token)
+
+	log.Printf("client: %+v", client)
+
+	return &PullerResult{}, nil
 }

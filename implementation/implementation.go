6773383fb6f1a1da83efe75e6e0adad7f22a968c
Chris Ramón <rchristian.ramon@gmail.com>
2025-02-10 17:32:57 -0500

    implementation: adds js ref implementation

--- implementation/implementation.go
+++ implementation/implementation.go
@@ -3,3 +3,5 @@ package implementation
 var Implementations = map[string]string{
 	"https://github.com/graphql-go/graphql": "https://github.com/graphql-go/graphql",
 }
+
+var JSRefImplementation string = "https://github.com/graphql/graphql-js/tree/v0.6.0"

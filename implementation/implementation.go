e8887fa00edb1a4dbf92c6a845ca4c703296cddb
Chris Ramón <rchristian.ramon@gmail.com>
2025-02-11 14:28:33 -0500

    implementation: consolidates Implementations using structs

--- implementation/implementation.go
+++ implementation/implementation.go
@@ -1,7 +1,31 @@
 package implementation
 
-var Implementations = map[string]string{
-	"https://github.com/graphql-go/graphql": "https://github.com/graphql-go/graphql",
+import (
+	"graphql-go/compatibility-unit-tests/types"
+)
+
+var GraphqlGoImplementation = types.Implementation{
+	Repo: types.Repository{
+		Name:          "graphql-go-graphql",
+		URL:           "https://github.com/graphql-go/graphql",
+		ReferenceName: "refs/heads/master",
+	},
 }
 
-var JSRefImplementation string = "https://github.com/graphql/graphql-js"
+var GraphqlJSImplementation = types.Implementation{
+	Repo: types.Repository{
+		Name:          "graphql-graphql-js",
+		URL:           "https://github.com/graphql/graphql-js",
+		ReferenceName: "v0.6.0",
+	},
+}
+
+var Implementations = []types.Implementation{GraphqlGoImplementation}
+
+var gqlGoImplURL = GraphqlGoImplementation.Repo.URL
+var jsImplURL = GraphqlJSImplementation.Repo.URL
+
+var ImplementationsMap = map[string]types.Implementation{
+	gqlGoImplURL: GraphqlGoImplementation,
+	jsImplURL:    GraphqlJSImplementation,
+}

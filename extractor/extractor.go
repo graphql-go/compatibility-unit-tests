bca0f951054db23dbd00ce100901578b9c3e95f8
Chris Ramón <rchristian.ramon@gmail.com>
2025-02-17 16:20:04 -0500

    extractor: wires reading impl files

--- extractor/extractor.go
+++ extractor/extractor.go
@@ -2,6 +2,7 @@ package extractor
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/tdewolff/parse/v2"
 	"github.com/tdewolff/parse/v2/js"
@@ -17,7 +18,24 @@ type ExtractorParams struct {
 	Source string
 }
 
+func (e *Extractor) readFiles() (*ExtractorResult, error) {
+	entries, err := os.ReadDir("./repos/graphql-graphql-js")
+	if err != nil {
+		return nil, err
+	}
+
+	for _, e := range entries {
+		fmt.Println(e.Name())
+	}
+
+	return nil, nil
+}
+
 func (e *Extractor) Extract(params *ExtractorParams) (*ExtractorResult, error) {
+	if _, err := e.readFiles(); err != nil {
+		return nil, err
+	}
+
 	ast, err := js.Parse(parse.NewInputString(params.Source), js.Options{})
 	if err != nil {
 		return nil, err

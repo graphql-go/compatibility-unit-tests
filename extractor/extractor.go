efdcabe9041501c8df58f0b65d513e18084b0670
Chris Ramón <rchristian.ramon@gmail.com>
2025-02-17 15:36:35 -0500

    extractor: wires the javascript parse

--- extractor/extractor.go
+++ extractor/extractor.go
@@ -1,7 +1,10 @@
 package extractor
 
 import (
-  "github.com/robertkrimen/otto/parser"
+	"fmt"
+
+	"github.com/tdewolff/parse/v2"
+	"github.com/tdewolff/parse/v2/js"
 )
 
 type Extractor struct {
@@ -11,13 +14,22 @@ type ExtractorResult struct {
 }
 
 type ExtractorParams struct {
-  Source string
+	Source string
 }
 
 func (e *Extractor) Extract(params *ExtractorParams) (*ExtractorResult, error) {
-	if _, err := parser.ParseFile(nil, "", params.Source, 0); err != nil {
-    return nil, err
-  }
+	ast, err := js.Parse(parse.NewInputString(params.Source), js.Options{})
+	if err != nil {
+		return nil, err
+	}
+
+	for _, a := range ast.List {
+		str, ok := a.(*js.VarDecl)
+		if ok {
+			fmt.Printf("default: %+v\n", str.List[0].Default)
+			fmt.Printf("binding: %+v\n", str.List[0].Binding)
+		}
+	}
 
 	return &ExtractorResult{}, nil
 }

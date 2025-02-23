8c5c7aff6c378ad1c7efd983ed05cc03488a8271
Chris Ramón <rchristian.ramon@gmail.com>
2025-02-23 16:58:46 -0500

    puller-js: adds implementation for pulling tests names

--- puller-js/index.ts
+++ puller-js/index.ts
@@ -4,9 +4,22 @@ import * as ts from "typescript";
 const fileName = "schema-test.ts";
 const sourceFile = ts.createSourceFile(fileName, readFileSync(fileName).toString(), ts.ScriptTarget.ES2015);
 
+
+const tests = [] as any;
+
 function walk(node: ts.SourceFile | ts.Node) {
   const n = node as any;
-  console.log(n?.name?.escapedText);
+
+  if(n?.kind === ts.SyntaxKind.CallExpression) {
+    if(n?.arguments && n?.arguments.length) {
+      if(n?.arguments[0].text) {
+        if(n?.expression?.escapedText === "describe" || n?.expression?.escapedText === "it") {
+          const testName = n?.arguments[0].text;
+          tests.push(testName);
+        }
+      }
+    }
+  }
 
   node.forEachChild((subNode: ts.Node) => {
    walk(subNode);

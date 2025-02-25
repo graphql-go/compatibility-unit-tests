98b28178e1a0a917b32aa7d4d7d0fd333a09b7bf
Chris Ramón <rchristian.ramon@gmail.com>
2025-02-25 16:04:41 -0500

    index: prettier

--- puller-js/index.ts
+++ puller-js/index.ts
@@ -1,21 +1,27 @@
 import { readFileSync } from "fs";
 import * as ts from "typescript";
-import { camelCase, upperFirst } from 'lodash';
+import { camelCase, upperFirst } from "lodash";
 import * as fs from "fs";
 
 const fileName = "schema-test.ts";
-const sourceFile = ts.createSourceFile(fileName, readFileSync(fileName).toString(), ts.ScriptTarget.ES2015);
-
+const sourceFile = ts.createSourceFile(
+  fileName,
+  readFileSync(fileName).toString(),
+  ts.ScriptTarget.ES2015,
+);
 
 const tests = [] as any;
 
 function walk(node: ts.SourceFile | ts.Node) {
   const n = node as any;
 
-  if(n?.kind === ts.SyntaxKind.CallExpression) {
-    if(n?.arguments && n?.arguments.length) {
-      if(n?.arguments[0].text) {
-        if(n?.expression?.escapedText === "describe" || n?.expression?.escapedText === "it") {
+  if (n?.kind === ts.SyntaxKind.CallExpression) {
+    if (n?.arguments && n?.arguments.length) {
+      if (n?.arguments[0].text) {
+        if (
+          n?.expression?.escapedText === "describe" ||
+          n?.expression?.escapedText === "it"
+        ) {
           const testName = n?.arguments[0].text;
           tests.push(testName);
         }
@@ -24,30 +30,31 @@ function walk(node: ts.SourceFile | ts.Node) {
   }
 
   node.forEachChild((subNode: ts.Node) => {
-   walk(subNode);
+    walk(subNode);
   });
 }
 
 // walk(sourceFile);
 
-for(let i = 0; i <  tests.length; i++) {
+for (let i = 0; i < tests.length; i++) {
   const testName = upperFirst(camelCase(tests[i]));
   console.log(testName);
 }
 
 const files = [] as any;
-const dirName = '../repos/graphql-graphql-js';
+const dirName = "../repos/graphql-graphql-js";
 const walkDir = (dirName: any) => {
-  const files2 = fs.readdirSync(dirName, { withFileTypes: true })
-  	.filter((item) => {
-      if(item.isDirectory()) {
+  const files2 = fs
+    .readdirSync(dirName, { withFileTypes: true })
+    .filter((item) => {
+      if (item.isDirectory()) {
         // walkDir(item);
       }
       return !item.isDirectory();
     })
     .map((item) => {
       files.push(item.name);
-        return item.name;
+      return item.name;
     });
 };
 

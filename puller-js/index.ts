c261c645dcba52b022051b8c2a6bb22ce0560140
Chris Ramón <rchristian.ramon@gmail.com>
2025-02-26 12:42:59 -0500

    puller-js: consolidates TestFiles & TestNames implementation

--- puller-js/index.ts
+++ puller-js/index.ts
@@ -4,17 +4,75 @@ import { camelCase, upperFirst } from "lodash";
 import * as fs from "fs";
 
 const fileName = "schema-test.ts";
-const sourceFile = ts.createSourceFile(
-  fileName,
-  readFileSync(fileName).toString(),
-  ts.ScriptTarget.ES2015,
-);
+
+class TestFiles {
+  constructor() {}
+
+  extractor() {
+    const files = [] as any;
+    const rootDir = "../repos/graphql-graphql-js";
+    const walkDir = (dirName: any) => {
+      const dirNames = fs.readdirSync(dirName, { withFileTypes: true });
+
+      for (let i = 0; i < dirNames.length; i++) {
+        const item = dirNames[i];
+        const filePath = `${item.path}/${item.name}`;
+
+        if (item.name.startsWith(".")) {
+          continue;
+        }
+
+        if (!fs.existsSync(filePath)) {
+          continue;
+        }
+
+        if (item.isDirectory()) {
+          walkDir(filePath);
+          continue;
+        }
+
+        files.push(filePath);
+      }
+    };
+
+    walkDir(rootDir);
+
+    return files;
+  }
+}
+
+const tests = [] as any;
 
 class TestNames {
   constructor() {}
 
+  extractor(files: string[]) {
+    const result = [];
+
+    for (let i = 0; i < 100; i++) {
+      const fileName0 = files[i];
+      const fileName = "schema-test.ts";
+
+      const check = fileName0.includes("__tests__");
+      if (!check) {
+        continue;
+      }
+
+      const sourceFile = ts.createSourceFile(
+        fileName,
+        readFileSync(fileName).toString(),
+        ts.ScriptTarget.ES2015,
+      );
+
+      const testNames = this.walk(sourceFile);
+
+      result.push(...testNames);
+    }
+
+    return result;
+  }
+
   walk(node: ts.SourceFile | ts.Node) {
-    const tests = [] as any;
     const n = node as any;
 
     if (n?.kind === ts.SyntaxKind.CallExpression) {
@@ -45,39 +103,16 @@ class TestNames {
   }
 }
 
-class TestFiles {
-  constructor() {}
-
-  extractor() {
-    const files = [] as any;
-    const rootDir = "../repos/graphql-graphql-js";
-    const walkDir = (dirName: any) => {
-      const dirNames = fs.readdirSync(dirName, { withFileTypes: true });
-
-      for (let i = 0; i < dirNames.length; i++) {
-        const item = dirNames[i];
-        const filePath = `${item.path}/${item.name}`;
-        if (
-          item.isDirectory() &&
-          fs.existsSync(filePath) &&
-          !item.name.startsWith(".")
-        ) {
-          walkDir(filePath);
-        } else {
-          files.push(filePath);
-        }
-      }
-    };
-
-    walkDir(rootDir);
-
-    return files;
-  }
-}
-
 const testNames = new TestNames();
 const testFiles = new TestFiles();
 
 const testFilesResult = testFiles.extractor();
+const testNamesResult = testNames.extractor(testFilesResult);
 
-console.log(testFilesResult);
+let result = "";
+for (let i = 0; i < testNamesResult.length; i++) {
+  const testName = testNamesResult[i];
+  result += `${testName}\n`;
+}
+
+fs.writeFileSync("unit-tests.txt", result);

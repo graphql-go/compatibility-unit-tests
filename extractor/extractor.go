5a10e0edd6c5463465267d63d51297238702f40d
Chris Ramón <rchristian.ramon@gmail.com>
2025-02-27 16:51:22 -0500

    extractor: adds initial extract implementation

--- extractor/extractor.go
+++ extractor/extractor.go
@@ -1,5 +1,11 @@
 package extractor
 
+import (
+	"fmt"
+	"io/fs"
+	"path/filepath"
+)
+
 type Extractor struct {
 }
 
@@ -10,5 +16,25 @@ type ExtractorParams struct {
 }
 
 func (e *Extractor) Extract(params *ExtractorParams) (*ExtractorResult, error) {
+	files := []string{}
+
+	walk := func(s string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if !d.IsDir() {
+			fmt.Println(s)
+		}
+
+		files = append(files, s)
+
+		return nil
+	}
+
+	implementationRootDir := "./repos/graphql-go-graphql/"
+
+	filepath.WalkDir(implementationRootDir, walk)
+
 	return nil, nil
 }

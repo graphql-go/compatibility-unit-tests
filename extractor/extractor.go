7d2c6c0ad3399c782dc69a2d48a37724b5cbbc4f
Chris Ramón <rchristian.ramon@gmail.com>
2025-03-03 13:46:09 -0500

    extractor: refactors go related extractor code

--- extractor/extractor.go
+++ extractor/extractor.go
@@ -2,12 +2,7 @@ package extractor
 
 import (
 	"fmt"
-	"go/ast"
-	"go/parser"
-	"go/token"
-	"io/fs"
 	"os"
-	"path/filepath"
 	"strings"
 
 	"graphql-go/compatibility-unit-tests/types"
@@ -49,7 +44,8 @@ func (e *Extractor) implementationTestNames(params ImplTestNamesParams) (map[typ
 		impl := params.Implementations[i]
 		switch impl.Type {
 		case types.GoImplementationType:
-			testNames, err := e.goTestNames(impl.Repo.Dir)
+			goExtractor := GoExtractor{}
+			testNames, err := goExtractor.TestNames(impl.Repo.Dir)
 			if err != nil {
 				return nil, err
 			}
@@ -76,92 +72,6 @@ func (e *Extractor) implementationTestNames(params ImplTestNamesParams) (map[typ
 	}
 
 	return result, nil
-}
-
-func (e *Extractor) testFiles(rootDir string) ([]string, error) {
-	testFiles := []string{}
-
-	walk := func(s string, d fs.DirEntry, err error) error {
-		if err != nil {
-			return err
-		}
-
-		if d.IsDir() {
-			return nil
-		}
-
-		if strings.HasSuffix(s, "_test.go") {
-			testFiles = append(testFiles, s)
-		}
-
-		return nil
-	}
-
-	filepath.WalkDir(rootDir, walk)
-
-	return testFiles, nil
-}
-
-func (e *Extractor) readFile(filePath string) (*os.File, error) {
-	goFile, err := os.Open(filePath)
-	if err != nil {
-		return nil, err
-	}
-
-	return goFile, nil
-}
-
-func (e *Extractor) readFuncNames(filePath string) ([]string, error) {
-	goFile, err := e.readFile(filePath)
-	if err != nil {
-		return nil, err
-	}
-	defer goFile.Close()
-
-	funcNames := []string{}
-	fset := token.NewFileSet()
-	astFile, err := parser.ParseFile(fset, "", goFile, parser.ParseComments)
-	if err != nil {
-		return nil, err
-	}
-
-	for _, decl := range astFile.Decls {
-		switch t := decl.(type) {
-		case *ast.FuncDecl:
-			funcNames = append(funcNames, t.Name.Name)
-		}
-	}
-
-	return funcNames, nil
-}
-
-func (e *Extractor) testNames(testFiles []string) ([]string, error) {
-	result := []string{}
-
-	for _, filePath := range testFiles {
-		funcNames, err := e.readFuncNames(filePath)
-		if err != nil {
-			return result, err
-		}
-
-		result = append(result, funcNames...)
-	}
-
-	return result, nil
-}
-
-func (e *Extractor) goTestNames(rootDir string) ([]string, error) {
-	testFiles, err := e.testFiles(rootDir)
-	if err != nil {
-		return nil, err
-	}
-
-	testNames, err := e.testNames(testFiles)
-	if err != nil {
-		return nil, err
-	}
-
-	return testNames, nil
 }
 
 func (e *Extractor) refTestNames(impl types.Implementation) ([]string, error) {
---
+++ extractor/go.go
@@ -0,0 +1,99 @@
+package extractor
+
+import (
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+type GoExtractor struct{}
+
+func (e *GoExtractor) TestNames(rootDir string) ([]string, error) {
+	testFiles, err := e.readTestFiles(rootDir)
+	if err != nil {
+		return nil, err
+	}
+
+	testNames, err := e.readTestNames(testFiles)
+	if err != nil {
+		return nil, err
+	}
+
+	return testNames, nil
+}
+
+func (e *GoExtractor) readTestFiles(rootDir string) ([]string, error) {
+	testFiles := []string{}
+
+	walk := func(s string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if d.IsDir() {
+			return nil
+		}
+
+		if strings.HasSuffix(s, "_test.go") {
+			testFiles = append(testFiles, s)
+		}
+
+		return nil
+	}
+
+	filepath.WalkDir(rootDir, walk)
+
+	return testFiles, nil
+}
+
+func (e *GoExtractor) readTestNames(testFiles []string) ([]string, error) {
+	result := []string{}
+
+	for _, filePath := range testFiles {
+		funcNames, err := e.readFuncNames(filePath)
+		if err != nil {
+			return result, err
+		}
+
+		result = append(result, funcNames...)
+	}
+
+	return result, nil
+}
+
+func (e *GoExtractor) readFuncNames(filePath string) ([]string, error) {
+	goFile, err := e.readFile(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer goFile.Close()
+
+	funcNames := []string{}
+	fset := token.NewFileSet()
+	astFile, err := parser.ParseFile(fset, "", goFile, parser.ParseComments)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, decl := range astFile.Decls {
+		switch t := decl.(type) {
+		case *ast.FuncDecl:
+			funcNames = append(funcNames, t.Name.Name)
+		}
+	}
+
+	return funcNames, nil
+}
+
+func (e *GoExtractor) readFile(filePath string) (*os.File, error) {
+	goFile, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+
+	return goFile, nil
+}

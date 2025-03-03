92155dec62d53898b85e47c927bcfcbe7637ea78
Chris Ramón <rchristian.ramon@gmail.com>
2025-03-03 16:32:37 -0500

    main: adds result initial styling

--- main.go
+++ main.go
@@ -3,13 +3,24 @@ package main
 import (
 	"fmt"
 	"log"
+	"strconv"
 
 	mainApp "graphql-go/compatibility-unit-tests/app"
 	"graphql-go/compatibility-unit-tests/cmd"
 	"graphql-go/compatibility-unit-tests/implementation"
+
+	"github.com/charmbracelet/lipgloss"
 )
 
 var choices = []string{}
+
+var style = lipgloss.NewStyle().
+	Bold(true).
+	Foreground(lipgloss.Color("#FAFAFA")).
+	Background(lipgloss.Color("#7D56F4")).
+	PaddingTop(2).
+	PaddingLeft(4).
+	Width(22)
 
 func init() {
 	for _, i := range implementation.Implementations {
@@ -41,6 +52,9 @@ func main() {
 		log.Fatal(err)
 	}
 
-	log.Printf("successful tests count: %+v", len(result.SuccessfulTests))
-	log.Printf("failed tests count: %+v", len(result.FailedTests))
+	sTests := strconv.Itoa(len(result.SuccessfulTests))
+	fTests := strconv.Itoa(len(result.FailedTests))
+
+	log.Printf("successful tests count: %+v", style.Render(sTests))
+	log.Printf("failed tests count: %+v", style.Render(fTests))
 }
